ad7ca40cd7ed47d03c576f21e96f2ec16db38f58
Gaurav Prakash <[prakashgaurav189@gmail.com]>
2024-05-31 12:21:45 +0530

    Introduce runtime error

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-def add(a, b):
-    return a + b
-
 def divide(a, b):
-    if b == 0:
-        raise ValueError("Division by zero")
-    return a / b
-
-if __name__ == "__main__":
-    print("Add 1 + 2:", add(1, 2))
-    print("Divide 1 / 0:", divide(1, 0))
+    return a / 0  # Deliberate runtime error
